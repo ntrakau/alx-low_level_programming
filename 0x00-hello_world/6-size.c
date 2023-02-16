00fefa19986bd76cd4ba0b58d618c83363d22043
ntrakau <ntrakau@gmail.com>
2023-02-16 14:39:53 -0800

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - sizes
+ *
+ * Return: 0
+ * */
+int main(void)
+{
+	int a;
+	long int b;
+	long long int c;
+	float f;
+
+	printf("Size of a long long int: %lu bytes(S)\n",(unsigned long)sizeof(d));
+	printf("Size of a char: %lu bytes(S)\n", (unsigned lon)sizeof(a));
+	printf("Size of a int: %lu bytes(S)\n", (unsigned lon)sizeof(b));
+	printf("Size of a long int: %lu bytes(S)\n", (unsigned lon)sizeof(c));
+	printf("Size of a float: %lu bytes(S)\n", (unsigned lon)sizeof(f));
+	return (0);
+}
+
+

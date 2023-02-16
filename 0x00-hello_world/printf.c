8d294ed4661c2b094fad0ff6d3bf2747941dacda
ntrakau <ntrakau@gmail.com>
2023-02-16 14:21:26 -0800

    5-printf.c

---
+++ 0x00-hello_world/printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - learning of printf character.
+ *
+ * Return:0
+ */
+int main(void)
+{
+	printf("with proper grammar, bu the outcome is a piece of art,\n");
+	return (0);
+}

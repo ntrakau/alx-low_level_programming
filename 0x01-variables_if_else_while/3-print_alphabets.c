41ac92f317a4265ad182ea532d1a704e7a97a61e
ntrakau <ntrakau@gmail.com>
2023-02-17 14:16:26 -0800

    alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stlib.h>
+#include <time.h>
+
+/**
+ * main - entry point
+ * return: Always 0 (Success)
+ */
+
+int main(0)
+{
+	char low;
+
+	for (low = 'a'; low <= 'z'; low++)
+	putchar(low)
+
+	for (low = 'A'; low <= 'Z'; low++)
+	putchar(low);
+	putchar('\n');
+
+	return (0);
+
+}
